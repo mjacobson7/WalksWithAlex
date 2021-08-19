164d13b6db5d04ad1b0d405bb20359e975c60e7e
Max Jacobson <mjacobson7@gmail.com>
2021-08-19 09:36:10 -0600

    Add transitions to FAQ section

--- scripts/index.js
+++ scripts/index.js
@@ -3,19 +3,15 @@ const faqs = document.querySelectorAll('.faq__button');
 
 
 // Functions
-const toggleFaqVisibility = (faqAnswer, faqs) => {
-	faqs.forEach(faq => {
-		if (!faqAnswer.isEqualNode(faq.nextElementSibling)) {
-			faq.nextElementSibling.classList.remove('faq__item_show');
-		}
-	})
+const toggleFaqVisibility = (faqAnswer, chevron) => {
 	faqAnswer.classList.toggle('faq__item_show');
+	chevron.classList.toggle('faq__chevron_rotate');
 }
 
 
 // Event Listeners
 faqs.forEach(faq => {
 	faq.addEventListener('click', (e) => {
-		toggleFaqVisibility(e.currentTarget.nextElementSibling, faqs)
+		toggleFaqVisibility(e.currentTarget.nextElementSibling, e.currentTarget.querySelector('.faq__chevron'))
 	})
 })
