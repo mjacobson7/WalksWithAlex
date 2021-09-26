4ee5abe273363191e3c96889957d0736cc49faff
Max Jacobson <mjacobson7@gmail.com>
2021-09-25 23:49:08 -0600

    Add 'read more/read less' buttons on services page

--- src/scripts/services.js
+++ src/scripts/services.js
@@ -1,1 +1,16 @@
 import '../pages/services.css';
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach((card) => {
+	const links = card.querySelectorAll('.card__link');
+	links.forEach((link) => {
+		link.addEventListener('click', () => {
+			card
+				.querySelector('.card__read-more')
+				.classList.toggle('card__read-more_visible');
+
+			card.querySelector('.card__dots').classList.toggle('card__dots_visible');
+		});
+	});
+});
